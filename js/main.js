141262ca35989f5e1dce45a318c7193bf90e2886
Марина Селях <maryna.seliakh@gmail.cpm>
2018-10-01 18:04:00 +0300

    change main slider

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.slider').slick({
-    	/*autoplay: true,
-        autoplaySpeed: 2000,*/
-        dots:true
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true
     });
-    
+    $('.go_to').click(function () {
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({scrollTop: $(scroll_el).offset().top}, 1000);
+        }
+        return false;
+    });
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:3,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+                loop:true
+            }
+        }
+    });
 });
